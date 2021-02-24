565fbf407810fa6c6b9f361fd5495e14706b4cf4
sachinshrestha <sachinshrestha483@gmail.com>
2021-02-24 11:19:40 +0530

    initial

---
+++ precache-manifest.30bef3e7c55ff8da71f597eda1371d7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2217c935dec4d55c047",
+    "url": "/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "60734deb37f6edd720e62c0cb6f3db60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/js/about.90b284f2.js"
+  },
+  {
+    "revision": "d2217c935dec4d55c047",
+    "url": "/js/app.03cbf88f.js"
+  },
+  {
+    "revision": "43978a68875bcc81af8d",
+    "url": "/js/chunk-vendors.8ccf7495.js"
+  },
+  {
+    "revision": "015e05f0e1e50e326ea3e67e3c8799ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
